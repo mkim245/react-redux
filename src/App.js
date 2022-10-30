6cb39e1b4a82442d5c7960ae9fde8f3629ea45dd
Mingu Kim <mkim245@gmail.com>
2022-10-29 20:18:43 -0400

    create containers and router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail'
 
 function App() {
   return (
     <div className="App">
-     <h1> Hello </h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetail} />
+          <Route>404 Not Found</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
