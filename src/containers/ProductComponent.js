b2563874521dcd6d67d065a566891226daa02899
Mingu Kim <mkim245@gmail.com>
2022-10-31 11:28:06 -0400

    add a link of a product

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom"
 import { useSelector } from "react-redux";
 
 export const ProductComponent = () => {
@@ -6,20 +7,22 @@ export const ProductComponent = () => {
   const renderList = products.map((product) => {
     const { id, title, image, price, category } = product;
     return (
-    <div className="four wide column" key={id}>
-      <div className="ui link cards">
-        <div className="card">
-          <div className="image">
-            <img src={image} alt={title} />
+      <div className="four wide column" key={id}>
+        <Link to={`/product/${id}`}>
+          <div className="ui link cards">
+            <div className="card">
+              <div className="image">
+                <img src={image} alt={title} />
+              </div>
+              <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta price">$ {price}</div>
+                <div className="meta">{category}</div>
+              </div>
+            </div>
           </div>
-          <div className="content">
-            <div className="header">{title}</div>
-            <div className="meta price">$ {price}</div>
-            <div className="meta">{category}</div>
-          </div>
-        </div>
+        </Link>
       </div>
-    </div>
     );
   });
   return <>{renderList}</>;
