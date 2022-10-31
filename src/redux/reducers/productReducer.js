b313bb6a802f17912666e9ae9c8eb58a587b3af5
Mingu Kim <mkim245@gmail.com>
2022-10-31 14:06:04 -0400

    show the selected image

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -17,7 +17,9 @@ export const selectedProductReducer = (state={}, {type, payload}) => {
   switch (type) {
     case ActionTypes.SELECTED_PRODUCT:
       return { ...state, ...payload };
-      default:
-       return state;
+    case ActionTypes.REMOVE_SELECTED_PRODUCT:
+      return {};
+    default:
+      return state;
   }
 };
